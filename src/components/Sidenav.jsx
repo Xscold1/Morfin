b5639b173cbe16d61b4ec5abc6f2bd93e86523e4
Xscold1 <54209938+Xscold1@users.noreply.github.com>
2025-09-10 02:27:48 +0000

    add sidenav

---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,12 @@
+export default function Sidenav (){
+    return (
+        <aside>
+            <div className="h-full bg-blue shadow">
+                <ul className="font-bold">
+                    <li>Home</li>
+                    <li>Home</li>
+                </ul>
+            </div>
+        </aside>
+    )
+} 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import Footer from "@components/Footer";
 import SummaryCards from "@components/SummaryCards";
 import TransactionsTable from "@components/TransactionsTable";
 import useTransactions from "@hooks/useTransactions";
+import Sidenav from '@components/Sidenav';
 
 export default function Dashboard() {
   const transactions = useTransactions();
@@ -15,13 +16,20 @@ export default function Dashboard() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col">
-      <Header />
-      <main className="flex-1 p-4">
-        <SummaryCards data={summary} />
-        <TransactionsTable transactions={transactions} />
-      </main>
-      <Footer />
+    <div className="flex">
+      <div className="w-1/4">
+        <Sidenav/>
+      </div>
+
+      <div className="min-h-screen w-full bg-gray-100 flex flex-col">
+        <Header />
+        <main className="flex-1 p-4">
+          <SummaryCards data={summary} />
+          <TransactionsTable transactions={transactions} />
+        </main>
+        <Footer />
+      </div>
+ 
     </div>
   );
 }
